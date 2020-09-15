91855ab0a924191d740386e9c991c4ff9dc61ac7
Asfar Shah <sasfar@users.noreply.github.com>
2020-09-14 22:29:28 -0400

    changing the org name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "SAASGlobal"
 
     workspaces {
       name = "learn-terraform-pipelines-k8s"
@@ -12,4 +12,5 @@ provider "google" {
   version = "3.10.0"
   project = var.google_project
   region  = var.region
+  #credentials = "${file("${var.GCLOUD_KEYFILE_JSON}")}"
 }
